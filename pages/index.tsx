3155bf26e1e25e5daee704fcb0326ddba9993ed6
ST10033851 <ST10033851@vcconnect.edu.za>
2023-07-22 07:31:54 +0200

    final commit

---
+++ pages/index.tsx
@@ -0,0 +1,44 @@
+import { NextPageContext } from 'next';
+import {getSession} from 'next-auth/react';
+import Navbar from '@/components/Navbar';
+import Billboard from '@/components/Billboard';
+import MovieList from '@/components/MovieList';
+import useMovieList from '@/hooks/useMovieList';
+import useFavorites from '@/hooks/useFavorites';
+import InfoModel from '@/components/InfoModel';
+import useInfoModel from '@/hooks/useInfoModel';
+
+export async function getServerSideProps(context:NextPageContext){
+  const session = await getSession(context);
+
+  if(!session){
+    return {
+      redirect:{
+        destination: '/auth',
+        permanent: false,
+      }
+    }
+  }
+
+  return{
+    props:{}
+  }
+}
+
+export default function Home() {
+  const { data:movies = []} = useMovieList();
+  const { data:favorites = []} = useFavorites();
+  const { isOpen, closeModel} = useInfoModel();
+  return (
+    <>
+      <InfoModel visible= {isOpen} onClose={closeModel}/>
+      <Navbar />
+      <Billboard />
+      <div className='pb-40'>
+        <MovieList title = "Trending now" data = {movies}/>
+        <MovieList title = "My List" data = {favorites}/>
+      </div>
+      
+    </>
+  )
+}
